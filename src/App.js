418f73694e3a85dbcf07f3c9cc52780ec0aeced5
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-23 16:17:29 +0100

    Working on Hall of fame.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css'
 import GuessCount from './GuessCount'
 import Card from './Card'
 import HallOfFame, { FAKE_HOF } from './HallOfFame'
+import HighScoreInput from "./HighScoreInput";
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
@@ -16,7 +17,13 @@ class App extends Component {
     cards: this.generateCards(),
     currentPair: [],
     guesses: 0,
+    hallOfFame: null,
     matchedCardIndices: [],
+  }
+
+  // Arrow fx for binding
+  displayHallOfFame = hallOfFame => {
+    this.setState({ hallOfFame })
   }
 
   generateCards() {
@@ -73,7 +80,7 @@ class App extends Component {
   }
 
   render() {
-    const { cards, guesses, matchedCardIndices } = this.state
+    const { cards, guesses, hallOfFame, matchedCardIndices } = this.state
     const won = matchedCardIndices.length === cards.length
     return (
       <div className="memory">
@@ -86,8 +93,10 @@ class App extends Component {
             index={index}
             onClick={this.handleCardClick}
           />
-        ))} 
-        { won && <HallOfFame entries={FAKE_HOF}/>}
+        ))}
+        {won && (
+          hallOfFame ? <HallOfFame entries={hallOfFame} /> : <HighScoreInput guesses={guesses} onStored={this.displayHallOfFame} />
+        )}
       </div>
     )
   }
