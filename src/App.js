5095938ba73526d57c97814d2740d623a8e8c0f4
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-23 01:10:06 +0100

    Memory cards working.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
 
 class App extends Component {
   state = {
@@ -37,13 +38,38 @@ class App extends Component {
       return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
     }
     if (currentPair.includes(index)) {
-      
+      return indexMatched ? 'justMatched' : 'justMismatched'
     }
+    return indexMatched ? 'visible' : 'hidden'
   }
 
   // Arrow function for binding
-  handleCardClick = card => {
-    console.log(card, "clicked", this)
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
   }
 
   render() {
@@ -53,7 +79,13 @@ class App extends Component {
       <div className="memory">
         <GuessCount guesses={guesses} />
         {cards.map((card, index) => (
-          <Card card={card} feedback="visible" key={index} onClick={this.handleCardClick} />
+          <Card 
+            card={card}
+            feedback={this.getFeedbackForCard(index)}
+            key={index}
+            index={index}
+            onClick={this.handleCardClick}
+          />
         ))} 
         { won && <HallOfFame entries={FAKE_HOF}/>}
       </div>
