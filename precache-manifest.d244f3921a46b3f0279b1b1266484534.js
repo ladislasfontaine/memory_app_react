a2973e57fc882fa4d45b1289f4ae0ed73532db66
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-01-02 17:32:01 +0100

    Updates

---
+++ precache-manifest.d244f3921a46b3f0279b1b1266484534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c146bf217c3d0f3c447b2296ef4c6a",
+    "url": "/memory_app_react/index.html"
+  },
+  {
+    "revision": "2604f0d27ee298d87e71",
+    "url": "/memory_app_react/static/css/main.a2a711b9.chunk.css"
+  },
+  {
+    "revision": "8590a22810647f05dc11",
+    "url": "/memory_app_react/static/js/2.b31c60ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory_app_react/static/js/2.b31c60ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2604f0d27ee298d87e71",
+    "url": "/memory_app_react/static/js/main.4e938ef6.chunk.js"
+  },
+  {
+    "revision": "8280d5ecf2fa34f9b545",
+    "url": "/memory_app_react/static/js/runtime-main.d3828a57.js"
+  }
+]);
